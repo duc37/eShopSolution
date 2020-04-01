057441ddc4c75cea7a4b2ba9b6b6ab7de7099c82
đức đặng trọng <dangtrongduc02@outlook.com>
2020-04-01 22:46:27 +0700

    seeding4/1

---
+++ eShopSolution.Data/Extentions/OnModelCreating.cs
@@ -0,0 +1,148 @@
+﻿using eShopSolution.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace eShopSolution.Data.Extentions
+{
+    public static class OnModelCreating
+    {
+        public static void Seed(this ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<AppConfig>().HasData(
+               new AppConfig() { 
+                   Key = "HomeTitle", 
+                   Value = "this is home of eShop"
+               },
+               new AppConfig() { 
+                   Key = "HomeKeyword", 
+                   Value = "this is HomeKeyword of eShop"
+               },
+               new AppConfig() { 
+                   Key = "HomeDescription", 
+                   Value = "this is HomeDescription of eShop" 
+               }
+               );
+
+            modelBuilder.Entity<Language>().HasData(
+                    new Language() {
+                        Id = "vi-VN",
+                        Name = "Tiếng việt", 
+                        IsDefault = true 
+                    },
+                    new Language() { 
+                        Id = "en-US", 
+                        Name = "English",
+                        IsDefault = false 
+                    }
+                );
+
+
+            modelBuilder.Entity<Category>().HasData(
+              new Category() { 
+                  Id=1,
+                  IsShowOnHome=true,
+                  ParentId=null,
+                  SortOrder=1,
+                  Status=Enums.Status.Active
+              },
+              new Category()
+              {
+                  Id=2,
+                  IsShowOnHome = true,
+                  ParentId = null,
+                  SortOrder = 2,
+                  Status = Enums.Status.Active
+
+              }
+
+              );
+            modelBuilder.Entity<CategoryTranslation>().HasData(
+                  new CategoryTranslation()
+                  {
+                      Id=1,
+                      CategoryId=1,
+                      Name = "Áo Nam",
+                      LanguageId = "vi-VN",
+                      SeoAlias = "ao-nam",
+                      SeoDescription = "Sản phẩm áo thời trang nam",
+                      SeoTitle = "Sản phẩm áo thời trang nam"
+                  },
+                      new CategoryTranslation()
+                      {
+                          Id=2,
+                          CategoryId = 1,
+                          Name = "Men Shirt",
+                          LanguageId = "en-US",
+                          SeoAlias = "men-sirt",
+                          SeoDescription = "The sirt product for men",
+                          SeoTitle = "The sirt product for men"
+                      },
+                       new CategoryTranslation()
+                       {
+                           Id=3,
+                           CategoryId=2,
+                           Name = "Áo Nữ",
+                           LanguageId = "vi-VN",
+                           SeoAlias = "ao-nu",
+                           SeoDescription = "Sản phẩm áo thời trang nữ",
+                           SeoTitle = "Sản phẩm áo thời trang nữ"
+                       },
+                      new CategoryTranslation()
+                      {
+                          Id=4,
+                          CategoryId=2,
+                          Name = "Woment Shirt",
+                          LanguageId = "en-US",
+                          SeoAlias = "woment-sirt",
+                          SeoDescription = "The sirt product for Woment",
+                          SeoTitle = "The sirt product for Woment"
+                      }
+
+                );
+
+            modelBuilder.Entity<Product>().HasData(
+                new Product()
+                {
+                    Id=1,
+                    DateCreated = DateTime.Now,
+                    OriginalPrice = 100000,
+                    Price = 20000,
+                    Stock = 0,
+                    ViewCount = 0,
+                }
+                );
+            modelBuilder.Entity<ProductTranslation>().HasData(
+                    
+                        new ProductTranslation(){
+                            Id=1,
+                            ProductId=1,
+                            Name="Áo Nam",
+                            LanguageId="vi-VN",
+                            SeoAlias="ao-nam",
+                            SeoDescription="Sản phẩm áo thời trang nam",
+                            SeoTitle="Sản phẩm áo thời trang nam",
+                            Details="test"
+                        },
+                        new ProductTranslation(){
+                            Id=2,
+                            ProductId=1,
+                            Name="Men Shirt",
+                            LanguageId="en-US",
+                            SeoAlias="men-sirt",
+                            SeoDescription="The sirt product for men",
+                            SeoTitle="The sirt product for men",
+                            Description=""
+                        }
+                );
+            modelBuilder.Entity<ProductInCategory>().HasData(
+                     new ProductInCategory()
+                     {
+                         ProductId = 1,
+                         CategoryId=1
+                     }
+                );
+        }
+    }
+}
---
+++ eShopSolution.Data/Migrations/20200401151210_SeeDate.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace eShopSolution.Data.Migrations
+{
+    public partial class SeeDate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "SeoAlias",
+                table: "Products");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                nullable: false,
+                defaultValue: new DateTime(2020, 4, 1, 22, 12, 10, 248, DateTimeKind.Local).AddTicks(9062),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValue: new DateTime(2020, 3, 20, 12, 45, 45, 253, DateTimeKind.Local).AddTicks(937));
+
+            migrationBuilder.InsertData(
+                table: "AppConfigs",
+                columns: new[] { "Key", "Value" },
+                values: new object[,]
+                {
+                    { "HomeTitle", "this is home of eShop" },
+                    { "HomeKeyword", "this is HomeKeyword of eShop" },
+                    { "HomeDescription", "this is HomeDescription of eShop" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Categories",
+                columns: new[] { "Id", "IsShowOnHome", "ParentId", "SortOrder", "Status" },
+                values: new object[,]
+                {
+                    { 1, true, null, 1, 1 },
+                    { 2, true, null, 2, 1 }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Languages",
+                columns: new[] { "Id", "IsDefault", "Name" },
+                values: new object[,]
+                {
+                    { "vi-VN", true, "Tiếng việt" },
+                    { "en-US", false, "English" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Products",
+                columns: new[] { "Id", "DateCreated", "OriginalPrice", "Price" },
+                values: new object[] { 1, new DateTime(2020, 4, 1, 22, 12, 10, 265, DateTimeKind.Local).AddTicks(9673), 100000m, 20000m });
+
+            migrationBuilder.InsertData(
+                table: "CategoryTranslations",
+                columns: new[] { "Id", "CategoryId", "LanguageId", "Name", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, 1, "vi-VN", "Áo Nam", "ao-nam", "Sản phẩm áo thời trang nam", "Sản phẩm áo thời trang nam" },
+                    { 3, 2, "vi-VN", "Áo Nữ", "ao-nu", "Sản phẩm áo thời trang nữ", "Sản phẩm áo thời trang nữ" },
+                    { 2, 1, "en-US", "Men Shirt", "men-sirt", "The sirt product for men", "The sirt product for men" },
+                    { 4, 2, "en-US", "Woment Shirt", "woment-sirt", "The sirt product for Woment", "The sirt product for Woment" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "ProductInCategories",
+                columns: new[] { "CategoryId", "ProductId" },
+                values: new object[] { 1, 1 });
+
+            migrationBuilder.InsertData(
+                table: "ProductTranslations",
+                columns: new[] { "Id", "Description", "Details", "LanguageId", "Name", "ProductId", "SeoAlias", "SeoDescription", "SeoTitle" },
+                values: new object[,]
+                {
+                    { 1, null, "test", "vi-VN", "Áo Nam", 1, "ao-nam", "Sản phẩm áo thời trang nam", "Sản phẩm áo thời trang nam" },
+                    { 2, "", null, "en-US", "Men Shirt", 1, "men-sirt", "The sirt product for men", "The sirt product for men" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeDescription");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeKeyword");
+
+            migrationBuilder.DeleteData(
+                table: "AppConfigs",
+                keyColumn: "Key",
+                keyValue: "HomeTitle");
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "CategoryTranslations",
+                keyColumn: "Id",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "ProductInCategories",
+                keyColumns: new[] { "CategoryId", "ProductId" },
+                keyValues: new object[] { 1, 1 });
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "ProductTranslations",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Categories",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "en-US");
+
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Id",
+                keyValue: "vi-VN");
+
+            migrationBuilder.DeleteData(
+                table: "Products",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.AddColumn<string>(
+                name: "SeoAlias",
+                table: "Products",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "OrderDate",
+                table: "Orders",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(2020, 3, 20, 12, 45, 45, 253, DateTimeKind.Local).AddTicks(937),
+                oldClrType: typeof(DateTime),
+                oldDefaultValue: new DateTime(2020, 4, 1, 22, 12, 10, 248, DateTimeKind.Local).AddTicks(9062));
+        }
+    }
+}
